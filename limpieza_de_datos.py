00cde21eb94b759a9b8c87793a9dbeed67e33229
Haro Belman <enrique.haro@uabc.edu.mx>
2024-11-18 17:08:01 -0800

    limpieza

---
+++ limpieza_de_datos.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('/Users/danny/OneDrive/Escritorio/airline_haro/train_airlines_delay_challenge.csv')
+
